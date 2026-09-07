0ca69e099b0b0e567e944d08393d463be0659c47
maiduan2589-prog <maiduan2589@gmail.com>
2026-09-07 12:22:06 +0700

    Export generation service API

--- src/generation/__init__.py
+++ src/generation/__init__.py
@@ -1,3 +1,10 @@
 from .models import GenerationJob, GenerationJobStatus
+from .service import GenerationRequest, GenerationResult, GenerationService
 
-__all__ = ["GenerationJob", "GenerationJobStatus"]
+__all__ = [
+    "GenerationJob",
+    "GenerationJobStatus",
+    "GenerationRequest",
+    "GenerationResult",
+    "GenerationService",
+]
