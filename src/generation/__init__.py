754c51d43f7e73b4b62ca36c8f07eea66a5b08f0
maiduan2589-prog <maiduan2589@gmail.com>
2026-09-07 15:02:54 +0700

    Export generation job repository interfaces

--- src/generation/__init__.py
+++ src/generation/__init__.py
@@ -1,10 +1,13 @@
 from .models import GenerationJob, GenerationJobStatus
+from .repository import GenerationJobRepository, InMemoryGenerationJobRepository
 from .service import GenerationRequest, GenerationResult, GenerationService
 
 __all__ = [
     "GenerationJob",
+    "GenerationJobRepository",
     "GenerationJobStatus",
     "GenerationRequest",
     "GenerationResult",
     "GenerationService",
+    "InMemoryGenerationJobRepository",
 ]
